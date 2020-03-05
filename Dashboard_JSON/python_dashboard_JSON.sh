433cb7f83588f1ead9c6c62fc286c91fe999974b
caragian <60917088+caragian@users.noreply.github.com>
2020-03-05 08:43:40 +0100

    Update python_dashboard_JSON.sh

--- Dashboard_JSON/python_dashboard_JSON.sh
+++ Dashboard_JSON/python_dashboard_JSON.sh
@@ -6,8 +6,6 @@ virtualenv custom_dashboard
 sleep 5s
 /neteye/shared/icingaweb2/extras/python_virtual_env/custom_dashboard/bin/pip install configparser ldap3
 cd /root/git-rep
-git clone https://github.com/caragian/Dashboard.git
-cd
 cd neteye_community/Dashboard/Dashboard_JSON/
 chmod 775 insert_dashboard.py
 cp insert_dashboard.py /neteye/shared/icingaweb2/extras/custom_dashboard
