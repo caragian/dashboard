2764cb6804c455c389dce609f8b23fd32bb1adeb
caragian <60917088+caragian@users.noreply.github.com>
2020-03-05 13:28:55 +0100

    Update python_dashboard_JSON.sh

--- Dashboard_JSON/python_dashboard_JSON.sh
+++ Dashboard_JSON/python_dashboard_JSON.sh
@@ -1,16 +1,13 @@
 mkdir -p /neteye/shared/icingaweb2/extras/custom_dashboard
 mkdir /neteye/shared/icingaweb2/extras/python_virtual_env
 cd /neteye/shared/icingaweb2/extras/python_virtual_env
-yum install -y python-pip python-virtualenv
-virtualenv custom_dashboard
-sleep 5s
+virtualenv-3 custom_dashboard
 /neteye/shared/icingaweb2/extras/python_virtual_env/custom_dashboard/bin/pip install configparser ldap3
-cd /root/git-rep
-cd neteye_community/Dashboard/Dashboard_JSON/
+cd /neteye/shared/icingaweb2/extras/custom_dashboard
+cp <git_repo>/Dashboard_JSON/*.json .
+cp <git_repo>/Dashboard_JSON/*.py .
+
 chmod 775 insert_dashboard.py
-cp insert_dashboard.py /neteye/shared/icingaweb2/extras/custom_dashboard
-cp config.json /neteye/shared/icingaweb2/extras/custom_dashboard
-cp authentication.json /neteye/shared/icingaweb2/extras/custom_dashboard
 
 
 
